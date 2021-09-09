50cc9725e00ab7da71a75f69d27cadb481eecd70
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-09-09 23:21:35 +0700

    đổi cái count lại thành 8

--- ecourses/ecourses/settings.py
+++ ecourses/ecourses/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-'PAGE_SIZE': '3',
+'PAGE_SIZE': '8',
 'DEFAULT_AUTHENTICATION_CLASSES': [
     'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ]
