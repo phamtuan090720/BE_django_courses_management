91bb5c8791a261fa3b690e98e7449f2a0311babb
thuthuy237 <thuthuy237@gmail.com>
2021-09-13 22:00:13 +0700

    up

--- ecourses/courses/views.py
+++ ecourses/courses/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse, Http404
 from rest_framework import viewsets, permissions, generics
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, JSONParser
 from rest_framework.views import APIView
 
 from .paginator import *
@@ -19,8 +19,8 @@ class AuthInfo(APIView):
 class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
-    parser_classes = [MultiPartParser, ]
-    # permission_classes = [permissions.IsAuthenticated()]
+    parser_classes = [MultiPartParser, JSONParser]
+    permission_classes = [UserPermission]
 
     @action(methods=['get'], detail=False, url_path="current-user", url_name='get-current-user')
     def get_current_user(self, request):
@@ -32,8 +32,73 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
             u = User.objects.get(pk=request.user.id)
             u.is_active = False
             u.save()
-            return Response(status=status.HTTP_200_OK, data="done")
-        except: return Response(status=status.HTTP_400_BAD_REQUEST, data="failed")
+            return Response(status=status.HTTP_200_OK, data="Successfully")
+        except: return Response(status=status.HTTP_400_BAD_REQUEST, data="Failed")
+
+    @action(methods=['get'], detail=False, url_path="upgrade-user", url_name="upgrade-user")
+    def upgrade_user(self, request):
+        try:
+            u = User.objects.get(pk=request.user.id)
+            t = Teacher.objects.get_or_create(user=u)
+            if not(t[1]):
+                return Response(status=status.HTTP_200_OK, data="User was an instructor account")
+            t[0].save()
+            return Response(status=status.HTTP_200_OK, data="Successfully")
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data="Failed")
+
+    @action(methods=['post'], detail=False, url_path="access-course", url_name="access-course")
+    def access_course(self, request):
+        try:
+            u = User.objects.get(pk=request.user.id)
+            course_id = request.data['course_id']
+            course = Course.objects.get(pk=course_id)
+            if not course is not None:
+                return Response(status=status.HTTP_400_BAD_REQUEST, data="Don't have any cousrse pk = "+course_id)
+            access = Student_Course.objects.get_or_create(student=u, course=course)
+            if not(access[1]):
+                return Response(status=status.HTTP_200_OK, data="User joined this course")
+            access[0].save()
+            return Response(status=status.HTTP_200_OK, data="Access to course successfully")
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data='Failed')
+
+                # điều kiện để rating?
+    @action(methods=['post'], detail=False, url_path="follow", url_name="follow")
+    def follow(self, request):
+        try:
+            u = User.objects.get(pk=request.user.id)
+            teacher_id = request.data['teacher_id']
+            t = Teacher.objects.get(user=teacher_id)
+            if not t is not None:
+                return Response(status=status.HTTP_400_BAD_REQUEST, data="Don't have any teacher pk = " +teacher_id)
+            follow = Follow.objects.get_or_create(student=u, teacher=t)
+            if not (follow[1]):
+                return Response(status=status.HTTP_200_OK, data="You already follow this account")
+            follow[0].save()
+            return Response(status=status.HTTP_200_OK, data="Followed")
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data='Failed, Please check the request again')
+
+    @action(methods=['post'], detail=False, url_path="rating", url_name="rating")
+    def rating(self, request):
+        try:
+            u = User.objects.get(pk=request.user.id)
+            course_id = request.data['course_id']
+            try:
+                course = Course.objects.get(pk=course_id)
+            except: return Response(status=status.HTTP_400_BAD_REQUEST, data="Don't have any cousrse pk = " + course_id)
+            try:
+                join = Student_Course.objects.get(student=u, course=course)
+            except: return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data="User has not join this course")
+            point = request.data['rating_point']
+            if point <=5 and point >=0:
+                join.rate =point
+                join.save()
+                return Response(status=status.HTTP_200_OK, data="Access to course successfully")
+            else: return Response(status=status.HTTP_400_BAD_REQUEST, data="The rating points only accepts values from 0 to 5")
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data='Failed')
 
 
     def get_permissions(self):
@@ -46,23 +111,34 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
 class TeacherViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-    # permission_classes = [IsOwnerOrReadOnly]
+
+    @action(methods=['post'],detail=False, name='teacher', url_path='register-teacher', url_name='register-teacher')
+    def register_teacher(self, request):
+        try:
+            data = request.data
+            user = User.objects.create_user(username=data['username'],
+                                       email=data['email'],
+                                       password=data['password'])
+            user.set_password(data['password'])
+            user.save()
+            job = None
+            if data['job'] is not None:
+                job = Job.objects.get_or_create(name_job=data['job'])[0]
+                job.save()
+            teacher = Teacher.objects.create(user=user, job=job)
+            teacher.save()
+        except : return Response(status=status.HTTP_400_BAD_REQUEST, data = "create failed")
+
+        return Response(status=status.HTTP_201_CREATED, data=self.serializer_class(teacher).data)
 
 
-
-    # def get_permissions(self):
-    #     if(self.action=='list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
-
-class TagViewSet(viewsets.ViewSet):
+class TagViewSet(viewsets.ViewSet, generics.CreateAPIView, generics.RetrieveAPIView, generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     pagination_class = BasePaginator
 
 
-class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
+class CourseViewSet(viewsets.ViewSet, generics.ListAPIView, generics.UpdateAPIView, generics.CreateAPIView):
     queryset = Course.objects.all()
     query = Course.objects
     serializer_class = CourseSerializer
@@ -104,6 +180,7 @@ class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
         try:
             kw = request.query_params.get('kw', None)
             course = Course.objects.get(pk=pk)
+            self.check_object_permissions(request, course)
             queryset = course.lessons.all()
             if kw is not None:
                 queryset = queryset.filter(subject__icontains=kw)
@@ -148,110 +225,60 @@ class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
         return queryset
 
 
-class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
+class CategoryViewSet(viewsets.ViewSet, generics.CreateAPIView, generics.RetrieveAPIView, generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    # permission_classes = [permissions.IsAuthenticated]
-
-    # def get_permissions(self):
-    #     if(self.action=='list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
 
 class LessonViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
 
 class HomeWorkViewSet(viewsets.ViewSet):
     queryset = HomeWork.objects.all()
     serializer_class = HomeWorkSerializer
-    #
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 
 class FileViewSet(viewsets.ViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
-    #
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 
 class VideoViewSet(viewsets.ViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 class FollowViewSet(viewsets.ViewSet):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 class SkillViewSet(viewsets.ViewSet):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
 
-    # def get_permissions(self):
-        # if (self.action == 'list'):
-        #     return [permissions.AllowAny()]
-        # else:
-        #     return [permissions.IsAuthenticated()]
+
 class JobViewSet(viewsets.ViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 class GroupChatViewSet(viewsets.ViewSet):
     queryset = GroupChat.objects.all()
     serializer_class = GroupChatSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 class MessageViewSet(viewsets.ViewSet):
     queryset = Message.objects.all()
     serializer_class = MessSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 class JoinViewSet(viewsets.ViewSet):
     queryset = Student_Course.objects.all()
     serializer_class = JoinSerializer
 
-    # def get_permissions(self):
-    #     if (self.action == 'list' or self.action =='retrieve'):
-    #         return [permissions.AllowAny()]
-    #     else:
-    #         return [permissions.IsAuthenticated()]
+
 
 
 def index(request):
